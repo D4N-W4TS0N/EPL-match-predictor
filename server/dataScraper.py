1fbcb997bda660e8485eec58f210590d46980aa9
D4N-W4TS0N <danieltamawatson@gmail.com>
2025-03-14 22:27:54 +0000

    Refactor data scraping to iterate over multiple team URLs and handle missing shooting data, and write all data to a pandas DF and then a CSV file for later use by the machine learning algorithm

--- server/dataScraper.py
+++ server/dataScraper.py
@@ -2,6 +2,7 @@ import requests
 from bs4 import BeautifulSoup
 import pandas as pd
 import cloudscraper
+import time
 
 # Creates a class which will be used to fetch the data used in either displaying or predicting
 class DataScraper():
@@ -38,30 +39,50 @@ class DataScraper():
         # Creates a list of URLs for each team
         teamURLs = [f"https://fbref.com{link}" for link in links]
         
-        teamURL = teamURLs[0]
-        teamName = teamURL.split('/')[-1].replace('Stats', '').replace('-', ' ')
-        # teamSeason = year
-        print(teamName)
-        # Fetches the data from the first team URL
-        teamData = scraper.get(teamURL, headers=headers)
-        # Reads the data into a pandas dataframe
-        matches = pd.read_html(teamData.text, match='Scores & Fixtures')
-        # Parses the data
-        soup = BeautifulSoup(teamData.text, features='html.parser')
-        # Finds all the <a> tags
-        links = soup.find_all('a')
-        # Gets all the <a> tags that contain 'href' - a hyperlink
-        links = [link.get('href') for link in links]
-        # Only retains the links that link to shooting stats
-        links = [link for link in links if link and 'all_comps/shooting' in link]
+        counter = 0
+        allMatches = []
 
-        # Fetches the data from the first shooting URL
-        shootingData = scraper.get(f"https://fbref.com{links[0]}", headers=headers)
-        # Reads the data into a pandas dataframe
-        shooting = pd.read_html(shootingData.text, match='Shooting')[0]
-        # Drops the first level of the multi-index
-        shooting.columns = shooting.columns.droplevel()
-        # Merges the match data frame with select columns from the shootin data frame, joining each record on the 'Date' column
-        teamData = matches[0].merge(shooting[['Date', 'Sh', 'SoT', 'Dist', 'FK', 'PK', 'PKatt']], on='Date')
-        
-        print(teamData.head())
+        for teamURL in teamURLs:
+            teamName = teamURL.split('/')[-1].replace('Stats', '').replace('-', ' ')
+            # teamSeason = year
+            print(teamName)
+            # Fetches the data from the first team URL
+            teamData = scraper.get(teamURL, headers=headers)
+            # Reads the data into a pandas dataframe
+            matches = pd.read_html(teamData.text, match='Scores & Fixtures')
+            # Parses the data
+            soup = BeautifulSoup(teamData.text, features='html.parser')
+            # Finds all the <a> tags
+            links = soup.find_all('a')
+            # Gets all the <a> tags that contain 'href' - a hyperlink
+            links = [link.get('href') for link in links]
+            # Only retains the links that link to shooting stats
+            links = [link for link in links if link and 'all_comps/shooting' in link]
+
+            # Fetches the data from the first shooting URL
+            shootingData = scraper.get(f"https://fbref.com{links[0]}", headers=headers)
+            # Reads the data into a pandas dataframe
+            shooting = pd.read_html(shootingData.text, match='Shooting')[0]
+            # Drops the first level of the multi-index
+            shooting.columns = shooting.columns.droplevel()
+            try:
+                # Merges the match data frame with select columns from the shootin data frame, joining each record on the 'Date' column
+                teamData = matches[0].merge(shooting[['Date', 'Sh', 'SoT', 'Dist', 'FK', 'PK', 'PKatt']], on='Date')
+            except ValueError:
+                continue    
+
+            # Filters the data to only include Premier League matches
+            teamData = teamData[teamData['Comp'] == 'Premier League']
+            # Adds a column for the season
+            teamData['Season'] = "2024-2025"
+            # Adds a column for the team
+            teamData['Team'] = teamName
+
+            # Appends the data to the allMatches list
+            allMatches.append(teamData)
+
+            counter += 0.5
+            time.sleep(counter)
+
+        matchDF = pd.concat(allMatches)
+        matchDF.to_csv('server/matches.csv')
