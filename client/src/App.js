6a503bf31f53e25645959757d977d60e2517f25f
D4N-W4TS0N <danieltamawatson@gmail.com>
2025-03-28 15:07:59 +0000

    Add HomePage component and update routing; remove unused blank.js file

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import LoginForm from './components/LoginForm';
 import RegistrationForm from './components/RegistrationForm';
+import HomePage from './components/HomePage';
 
 
 function App() {
@@ -12,6 +13,7 @@ function App() {
           <Route path='/register' element={<RegistrationForm/>}/>
           <Route path='/login' element={<LoginForm />}/>
           <Route path='*' element={<LoginForm/>}/>
+          <Route path='/home' element={<HomePage/>}/>
         </Routes>
       </Router>
 
